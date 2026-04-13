177807c0b85e28c7cf74983e282e4f3f8c0321d3
Abdelcrks <abdelmoumenberkat@gmail.com>
2026-04-13 11:25:50 +0200

    feat: setup database and create all tables

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from database import engine, Base
+import models.offer
+import models.profile
+import models.project
+import models.skill
+import models.generation
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
